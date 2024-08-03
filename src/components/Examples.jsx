a16d0983b2a482160395422b0dac56541c4118a5
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-03 17:13:58 +0200

    feat: add Tabs component

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import TabButton from './TabButton.jsx';
 import { EXAMPLES } from '../data.js';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 
 export default function Examples(){
     const [ selectedTopic, setSelectedTopic ] = useState('');
@@ -23,21 +24,20 @@ export default function Examples(){
     
     return (
     <Section id="examples" title='Examples'>
-        <menu>
+      <Tabs buttons={<>
         <TabButton isSelected={selectedTopic === 'components' ? true : false} onClick={() => handleSelect('components')}>Components</TabButton>
         <TabButton isSelected={selectedTopic === 'jsx' ? true : false} onClick={() => handleSelect('jsx')}>JSX</TabButton>
         <TabButton isSelected={selectedTopic === 'props' ? true : false} onClick={() => handleSelect('props')}>Props</TabButton>
         <TabButton isSelected={selectedTopic === 'state' ? true : false} onClick={() => handleSelect('state')}>State</TabButton>
-        </menu>
-        {/* {selectedTopic === '' && <p>Please select a topic.</p>} Sintassi alternativa all'operatore ternario */}
-        {selectedTopic !== '' ? <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div> : <p>Please select a topic.</p>}
-        {/* {tabContent} Rendering condizionale con variabile */}
+        </>}>{selectedTopic !== '' ? <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div> : <p>Please select a topic.</p>}</Tabs>
+      {/* {selectedTopic === '' && <p>Please select a topic.</p>} Sintassi alternativa all'operatore ternario */}
+      {/* {tabContent} Rendering condizionale con variabile */}
     </Section>
     )
 }
