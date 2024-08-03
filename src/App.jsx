5af18f4f0777324fbd6206e49f1ae626338d7202
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-03 11:43:41 +0200

    feat: add conditional class rendering

--- src/App.jsx
+++ src/App.jsx
@@ -46,10 +46,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components' ? true : false} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx' ? true : false} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props' ? true : false} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state' ? true : false} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {/* {selectedTopic === '' && <p>Please select a topic.</p>} Sintassi alternativa all'operatore ternario */}
           {selectedTopic !== '' ? <div id="tab-content">
